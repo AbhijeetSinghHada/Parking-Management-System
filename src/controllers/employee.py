09f0d2130f6769dd48f8c2fe217c6dc51a35075c
Abhijeet Singh Hada <abhi22hada@gmail.com>
2023-10-05 09:34:33 +0530

    minor modifications in slots, vehicles Controllers

--- src/controllers/employee.py
+++
@@ -1,5 +0,0 @@
-class Employee:
-    def __init__(self,name,email_address,phone_number):
-        self.name = name
-        self.email_address = email_address
-        self.phone_number = phone_number
